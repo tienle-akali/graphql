840d7a4779ffabbf87599fca94f805df459c6144
Akali <ngoinhamaam1993@gmail.com>
2024-04-09 15:29:28 +0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = true;
-const isBasePath = false;
 
 const nextConfig = {
-  output: isProd === true ? "export" : undefined,
-  basePath: isBasePath === true ? "/out" : "",
-  //   assetPrefix: "/out",
+  output: "export",
+  basePath: "",
 };
 
 export default nextConfig;
