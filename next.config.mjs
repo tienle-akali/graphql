920cdf99cb1d369db13a3839170060d84fb0de8f
Akali <ngoinhamaam1993@gmail.com>
2024-04-09 17:25:33 +0700

    update config note

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 
 /**
  * - Nếu build chỉ riêng out trên github page thì export và để basePath là tên Repository chứa source out, ví dụ: basePath: "/guest",
- * - Nếu build nguyên source next js lên github hoặc gửi người khác source out để xem thì bỏ basePath
+ * - Nếu build nguyên source next js lên github hoặc gửi người khác source out để xem thì cũng không bị ảnh hưởng, bởi output và basePath dành cho việc build source static
  */
 const nextConfig = {
   output: "export",
